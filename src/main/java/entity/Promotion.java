eef067adef8adb147d24e9357c7b17a103348657
Eunjung <fkznsha23@gmail.com>
2024-01-31 14:55:44 +0900

    dd

--- src/main/java/entity/Promotion.java
+++ src/main/java/entity/Promotion.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,4 +21,7 @@ public class Promotion {
     private String condition;
     private LocalDate createDate;
     private LocalDate modifyDate;
+    private User adoptionUser;
+    private List<AdoptionForm> adoptionFormList;
+    private List<Diary> diaryList;
 }
